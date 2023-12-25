5ac85acf98bbbf1fd4b988df9c577303f6bd6c54
Yonel Ceruto <yonel.ceruto@scnd.com>
2023-12-25 11:14:25 -0500

    add Schema and Operation builders

---
+++ src/Builder/OperationBuilder.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace OpenSolid\OpenApiAssistantBundle\Builder;
+
+use Doctrine\Inflector\Inflector;
+use Doctrine\Inflector\InflectorFactory;
+use OpenApi\Annotations\Delete;
+use OpenApi\Annotations\Get;
+use OpenApi\Annotations\Items;
+use OpenApi\Annotations\JsonContent;
+use OpenApi\Annotations\MediaType;
+use OpenApi\Annotations\OpenApi;
+use OpenApi\Annotations\Parameter;
+use OpenApi\Annotations\Patch;
+use OpenApi\Annotations\PathItem;
+use OpenApi\Annotations\Post;
+use OpenApi\Annotations\Put;
+use OpenApi\Annotations\RequestBody;
+use OpenApi\Annotations\Response;
+use OpenApi\Annotations\Schema;
+use OpenApi\Generator;
+
+readonly class OperationBuilder
+{
+    private Inflector $inflector;
+
+    public function __construct(private SchemaBuilder $payloadToSchemaConverter)
+    {
+        $this->inflector = InflectorFactory::create()->build();
+    }
+
+    public function build(string $method, string $uri, ?string $req, ?string $res, OpenApi $openApi): void
+    {
+        if (Generator::isDefault($openApi->paths)) {
+            $openApi->paths = [];
+        }
+
+        if (isset($openApi->paths[$uri])) {
+            $pathItem = $openApi->paths[$uri];
+        } else {
+            $pathItem = new PathItem([]);
+            $pathItem->path = $uri;
+            $openApi->paths[$uri] = $pathItem;
+        }
+
+        $methodItem = $pathItem->{$method} = $this->buildMethodItem($method, $uri);
+
+        if (str_contains($uri, '{')) {
+            $this->buildParameters($uri, $methodItem);
+        }
+
+        if (null !== $req) {
+            $this->buildRequestBody($method, $uri, $req, $openApi, $methodItem);
+        }
+
+        $this->buildResponse($method, $uri, $res ?? '', $openApi, $methodItem);
+    }
+
+    private function guessSuccessStatusCode(string $method, bool $hasResponse = true): int
+    {
+        return match($method) {
+            'post' => 201,
+            'delete' => $hasResponse ? 200 : 204,
+            default => 200,
+        };
+    }
+
+    private function guessContentType(string $payload): string
+    {
+        return '[' === $payload[0] ? 'array' : 'object';
+    }
+
+    private function guessResourceName(string $uri): string
+    {
+        $parts = array_filter(explode('/', $uri), static fn (string $part): bool => '' !== $part);
+
+        if ([] === $parts) {
+            throw new \InvalidArgumentException(sprintf('Unable to guess resource name from URI "%s"', $uri));
+        }
+
+        $resource = array_shift($parts);
+
+        return ucfirst($this->inflector->singularize($resource));
+    }
+
+    private function buildMethodItem(string $method, string $uri): Put|Delete|Get|Patch|Post
+    {
+        $operationId = md5(strtoupper($method.'::'.$uri));
+
+        return match ($method) {
+            'get' => new Get(['operationId' => $operationId]),
+            'post' => new Post(['operationId' => $operationId]),
+            'put' => new Put(['operationId' => $operationId]),
+            'patch' => new Patch(['operationId' => $operationId]),
+            'delete' => new Delete(['operationId' => $operationId]),
+            default => throw new \InvalidArgumentException(sprintf('Unsupported method "%s"', $method)),
+        };
+    }
+
+    private function buildParameters(string $uri, Post|Patch|Get|Delete|Put $methodItem): void
+    {
+        $parameters = [];
+        foreach (explode('/', $uri) as $part) {
+            if (str_starts_with($part, '{') && str_ends_with($part, '}')) {
+                $parameter = new Parameter([
+                    'name' => strtolower(trim($part, '{}')),
+                    'in' => 'path',
+                    'required' => true,
+                    'schema' => new Schema(['type' => 'string']),
+                ]);
+                $parameters[] = $parameter;
+            }
+        }
+
+        if ([] === $parameters) {
+            return;
+        }
+
+        $methodItem->parameters = $parameters;
+    }
+
+    private function buildRequestBody(
+        string $method,
+        string $uri,
+        string $payload,
+        OpenApi $openApi,
+        Post|Patch|Get|Delete|Put $methodItem
+    ): void {
+        $resource = $this->guessResourceName($uri);
+        $name = $this->inflector->classify($method.' '.$resource.' Body');
+        $this->payloadToSchemaConverter->build($name, $payload, $openApi);
+
+        $content = new MediaType(['mediaType' => 'application/json']);
+        $content->schema = new Schema([]);
+        $content->schema->ref = '#/components/schemas/'.$name;
+
+        $requestBody = new RequestBody(['required' => true]);
+        $requestBody->merge([$content]);
+
+        $methodItem->requestBody = $requestBody;
+    }
+
+    private function buildResponse(
+        string $method,
+        string $uri,
+        string $payload,
+        OpenApi $openApi,
+        Post|Patch|Get|Delete|Put $methodItem
+    ): void {
+        $response = new Response([
+            'response' => $this->guessSuccessStatusCode($method, '' !== $payload),
+            'description' => 'Successful',
+        ]);
+
+        $resource = $this->guessResourceName($uri);
+        $name = $this->inflector->classify($method.' '.$resource.' View');
+
+        if ('' !== $payload) {
+            $this->payloadToSchemaConverter->build($name, $payload, $openApi);
+
+            $content = new MediaType(['mediaType' => 'application/json']);
+            $content->schema = new Schema([]);
+            $content->schema->type = $this->guessContentType($payload);
+            if ('array' === $content->schema->type) {
+                $content->schema->items = new Items([]);
+                $content->schema->items->ref = '#/components/schemas/'.$name;
+            } else {
+                $content->schema->ref = '#/components/schemas/'.$name;
+            }
+
+            $response->merge([$content]);
+        }
+
+        $methodItem->responses = [$response];
+    }
+}
---
+++ src/Builder/SchemaBuilder.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace OpenSolid\OpenApiAssistantBundle\Builder;
+
+use Doctrine\Inflector\Inflector;
+use Doctrine\Inflector\InflectorFactory;
+use OpenApi\Annotations\Components;
+use OpenApi\Annotations\Items;
+use OpenApi\Annotations\OpenApi;
+use OpenApi\Annotations\Property;
+use OpenApi\Annotations\Schema;
+use OpenApi\Generator;
+use OpenSolid\OpenApiAssistantBundle\OpenApi\OpenApiSpec;
+
+readonly class SchemaBuilder
+{
+    private Inflector $inflector;
+
+    public function __construct()
+    {
+        $this->inflector = InflectorFactory::create()->build();
+    }
+
+    public function build(string $name, string $payload, OpenApi $openApi): void
+    {
+        if (Generator::isDefault($openApi->components)) {
+            $openApi->components = new Components([]);
+            $openApi->components->schemas = [];
+        }
+
+        $this->process($name, json_decode($payload), $openApi->components);
+    }
+
+    protected function process(string $name, mixed $payload, Components $components): Schema
+    {
+        $schemaName = ucfirst($name);
+
+        if (is_object($payload)) {
+            $schema = new Schema(['type' => 'object']);
+            $schema->schema = $schemaName;
+            $schema->properties = [];
+        } elseif (is_array($payload)) {
+            $schema = new Schema(['type' => 'array']);
+        } else {
+            throw new \InvalidArgumentException(sprintf('Unsupported type "%s"', gettype($payload)));
+        }
+
+        foreach ($payload as $key => $value) {
+            if ('array' === $schema->type) {
+                if (is_scalar($value)) {
+                    $schema->items = new Items([]);
+                    $schema->items->type = gettype($value);
+                } elseif (is_object($value)) {
+                    $schema->items = new Items([]);
+                    $schema->items->type = 'object';
+                    $nestedSchema = $this->process($schemaName, $value, $components);
+                    $schema->items->ref = '#/components/schemas/'.$nestedSchema->schema;
+                }
+
+                return $schema;
+            }
+
+            if (is_object($value)) {
+                $nestedSchema = $this->process($schemaName.ucfirst($key), $value, $components);
+                $property = new Property(['property' => $key]);
+                $property->type = 'object';
+                $property->ref = '#/components/schemas/'.$nestedSchema->schema;
+            } elseif (is_array($value)) {
+                $property = new Property(['property' => $key]);
+                if ([] === $value) {
+                    $property->type = 'array';
+                    $property->items = new Items([]);
+                    $property->items->type = 'string';
+                } elseif (is_object($value[0])) {
+                    $property->type = 'array';
+                    $property->items = new Items([]);
+                    $property->items->type = 'object';
+                    $nestedSchema = $this->process($schemaName.ucfirst($this->inflector->singularize($key)), $value[0], $components);
+                    $property->items->ref = '#/components/schemas/'.$nestedSchema->schema;
+                } elseif (is_string($value[0]) || is_int($value[0])) {
+                    $property->type = gettype($value[0]);
+                    $property->enum = $value;
+                } else {
+                    // unknown type, skip
+                    continue;
+                }
+            } elseif (is_scalar($value)) {
+                $property = new Property(['property' => $key]);
+                $property->type = OpenApiSpec::getDataType($value);
+                if ($format = OpenApiSpec::getDataFormat($value)) {
+                    $property->format = $format;
+                }
+                if ($pattern = OpenApiSpec::getDataPattern($value)) {
+                    $property->pattern = $pattern;
+                }
+                if (!$pattern && 'password' !== $format) {
+                    $property->example = $value;
+                }
+            } else {
+                // unknown type, skip
+                continue;
+            }
+
+            $schema->properties[$key] = $property;
+        }
+
+        if (!isset($components->schemas[$schemaName])) {
+            $components->schemas[$schemaName] = $schema;
+        }
+
+        return $schema;
+    }
+}
