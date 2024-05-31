67dd4cbbaa074274467b049835ec8dd25e83b2b9
Yonel Ceruto <yonel.ceruto@scnd.com>
2024-05-31 08:32:25 -0400

    Set route example for demo

--- demo/config/routes.php
+++ demo/config/routes.php
@@ -5,4 +5,12 @@ use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 return static function (RoutingConfigurator $routes): void {
     $routes->import('@OpenApiBundle/config/routes.php');
     $routes->import('@OpenApiAssistantBundle/config/routes.php');
+
+//    $routes->import(
+//        resource: [
+//            'path' => '../src/User/Controller/',
+//            'namespace' => 'Demo\\User\\Controller\\',
+//        ],
+//        type: 'attribute',
+//    );
 };
